7522a2490aeea68ef151e3fd22480dc52ec196f9
kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>
2023-12-25 12:52:17 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 0.09 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.swift
+++ 프로그래머스/unrated/181905. 문자열 뒤집기/문자열 뒤집기.swift
@@ -1,21 +1,8 @@
 import Foundation
 
-func solution(_ my_string: String, _ s: Int, _ e: Int) -> String {
-    guard s >= 0 && e < my_string.count && s <= e else {
-        return "Invalid indices"
-    }
-    
-    var reversedSubstring = ""
-    let startIndex = my_string.index(my_string.startIndex, offsetBy: s)
-    let endIndex = my_string.index(my_string.startIndex, offsetBy: e)
-    let substring = my_string[startIndex...endIndex]
-    
-    for char in substring.reversed() {
-        reversedSubstring += String(char)
-    }
-    
-    let prefix = my_string.prefix(s)
-    let suffix = my_string.suffix(from: my_string.index(my_string.startIndex, offsetBy: e + 1))
-    
-    return "\(prefix)\(reversedSubstring)\(suffix)"
+func solution(_ my_string:String, _ s:Int, _ e:Int) -> String {
+    var my_string = Array(my_string)
+    my_string.replaceSubrange(s...e, with: Array(my_string[s...e].reversed()))
+    return String(my_string)
 }
+
