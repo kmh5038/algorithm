db44736b7b439232960265feb94fa15a4e36461b
kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>
2023-12-28 09:41:28 +0900

    [level unrated] Title: 9로 나눈 나머지, Time: 0.04 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.swift
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ number:String) -> Int {
-    var num = 0
-    for i in number {
-        if let number = Int(String(i)) {
-            num += number
-        } 
-    }
-    return num % 9
+    return number.map { Int(String($0))! }.reduce(0, +) % 9
 }
