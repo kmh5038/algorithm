f58bd05a328688cc2354803ab91cbb1b655cc255
kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>
2024-01-23 09:35:09 +0900

    [level 0] Title: 편지, Time: 0.01 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/120898. 편지/편지.swift
+++ 프로그래머스/0/120898. 편지/편지.swift
@@ -1,10 +1,8 @@
 import Foundation
 
-func solution(_ message: String) -> Int {
-    var result = 0
-    for i in message {
-        result += 2
-        print(i)
-    }
-    return result
+func solution(_ message:String) -> Int {
+        return message.count * 2
+
+
+
 }
