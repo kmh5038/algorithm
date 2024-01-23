42a4a23070ddfcf78a8aec3fb03c77eec6b377da
kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>
2024-01-23 09:15:41 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.swift
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.swift
@@ -1,13 +1,5 @@
 import Foundation
 
 func solution(_ num_list:[Int]) -> [Int] {
-    var result = [0,0]
-    for i in num_list {
-        if i % 2 == 0 {
-            result[0] += 1
-        } else {
-            result[1] += 1
-        }
-    }
-    return result
+    return [num_list.filter{$0 % 2 == 0}.count, num_list.filter{$0 % 2 == 1}.count]
 }
