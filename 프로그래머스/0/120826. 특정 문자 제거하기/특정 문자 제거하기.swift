3f350141051577f3e33ea54a0fad7d7bb8f11bd5
kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>
2024-01-30 09:20:47 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.06 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.swift
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.swift
@@ -1,6 +1,5 @@
 import Foundation
 
 func solution(_ my_string:String, _ letter:String) -> String {
-    let result = my_string.replacingOccurrences(of: letter, with: "")
-    return result
+    return my_string.filter{String($0) != letter}
 }
