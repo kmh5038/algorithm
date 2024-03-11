3de2ea5c26c44de4552861dc792a2ef508679a12
kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>
2024-03-11 09:28:13 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.02 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.swift
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.swift
@@ -1,10 +1,7 @@
 import Foundation
 
 func solution(_ n:Int) -> Int {
-    for i in 1...1000 {
-        if n == (i * i) {
-            return 1
-        } 
-    }
-    return 2
+    var num = Int(sqrt(Double(n)))
+    
+    return n == (num * num) ? 1 : 2
 }
