98cacfbaf9e4bc2a49f24819a153c5c1849fe949
kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>
2024-03-19 10:57:47 +0900

    [level 0] Title: 약수 구하기, Time: 0.12 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.swift
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.swift
@@ -1,11 +1,5 @@
 import Foundation
 
 func solution(_ n:Int) -> [Int] {
-    var result:[Int] = []
-    for i in 1...n {
-        if n % i == 0 {
-            result.append(i)
-        }
-    }
-    return result
+    (1...n).filter { n % $0 == 0}
 }
