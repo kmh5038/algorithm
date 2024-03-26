feb12422781e4334d3d41b04914d140919084dae
kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>
2024-03-26 11:41:19 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.04 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.swift
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.swift
@@ -1,17 +1,6 @@
 import Foundation
-//최대공약수 gcd 함수
-func gcd(_ a:Int, _ b:Int) -> Int {
-    if b == 0 {
-        return a
-    } else {
-        return gcd(b, a % b)
-    }
-}
-//최소공배수 lcm 함수
-func lcm(_ a:Int, _ b:Int) -> Int {
-    return a*b / gcd(a,b)
-}
 
 func solution(_ n:Int) -> Int {
-    return lcm(n,6) / 6
+    let div: [Int] = [6,3,2,1].filter { n % $0 == 0}
+    return n / div[0]
 }
