98eb0e60331ea3f3fee3d5401e019b191bb5a5ca
kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>
2024-03-27 09:53:55 +0900

    [level 0] Title: 369게임, Time: 0.06 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.swift
+++ 프로그래머스/0/120891. 369게임/369게임.swift
@@ -1,12 +1,5 @@
 import Foundation
 
 func solution(_ order:Int) -> Int {
-    var strOrder = String(order)
-    var result = 0
-    for i in strOrder {
-        if i == "3" || i == "6" || i == "9" {
-            result += 1
-        }
-    }
-    return result
+    return String(order).filter { "369".contains($0) }.count
 }
