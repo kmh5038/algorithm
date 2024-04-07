a404ee1d9ec4ec9f03472972f137d89468583c77
kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>
2024-04-07 17:37:57 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ numbers:[Int]) -> Int {
+    var sum = 0
+    
+    for i in 0...9 {
+        if !numbers.contains(i) {
+            sum += i
+        }
+    }
+    
+    return sum
+}
