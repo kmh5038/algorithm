8c65eb4ff23312b9bffce45be6c6a86afe6a3380
kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>
2024-05-16 10:53:44 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.swift
@@ -0,0 +1,10 @@
+func solution(_ x:Int) -> Bool {
+  var sum = 0
+    
+  for i in String(x) {
+    guard let number = Int(String(i)) else { break }
+    sum += number
+  }
+    
+  return x % sum == 0
+}
