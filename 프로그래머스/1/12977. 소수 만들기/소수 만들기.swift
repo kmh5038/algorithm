a5e19f89374ef162c9451f626e80ab8521e03295
kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>
2024-07-02 15:05:39 +0900

    [level 1] Title: 소수 만들기, Time: 0.02 ms, Memory: 16.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+func solution(_ nums:[Int]) -> Int {
+    
+    var result: Int = 0; 
+    
+    for i in 0..<nums.count {
+        for j in i..<nums.count {
+            for k in j..<nums.count {
+                if nums[i] != nums[j] && nums[j] != nums[k] { 
+                    var sum = nums[i] + nums[j] + nums[k]; 
+                    result += prime(sum); 
+                }
+            }
+        }
+    }
+
+    return result;
+}
+
+func prime(_ number: Int) -> Int{
+    var count: Int = 0;
+    
+    for i in 1...number {
+        if number % i == 0 {
+            count += 1;
+        }
+    }
+    
+    return count == 2 ? 1 : 0; 
+}
